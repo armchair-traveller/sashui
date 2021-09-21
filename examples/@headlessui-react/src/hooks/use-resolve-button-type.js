42b6f4fa08a61cc2be3a79a36c60e16abc822dd3
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-09-21 13:58:37 -0700

    comment on as, btn type -> js

---
+++ examples/@headlessui-react/src/hooks/use-resolve-button-type.js
@@ -0,0 +1,23 @@
+/**
+ * This seems to be used w/ `as` functionality, which doesn't exist in Svelte
+ *
+ * `svelte:element` dynamic elements is a relevant issue https://github.com/sveltejs/svelte/issues/2324 but it seems like
+ * it'll take a very long time before anyone actually implements it as it's quite niche and has many issues w/ its
+ * current proposed implementation, except for the simplest use case.
+ *
+ * This isn't a dealbreaker. The feature has a myriad of alternatives, some better than `as`.
+ * For example, an action-based API would be very suitable.
+ *
+ * Relevant: https://twitter.com/peduarte/status/1426181279293362176 thread has alternatives you can consider.
+ * */
+export function useResolveButtonType(type, ref) {
+  if (
+    !type &&
+    ref &&
+    ref instanceof HTMLButtonElement &&
+    !ref.hasAttribute('type')
+  ) {
+    return 'button'
+  }
+  return type
+}
