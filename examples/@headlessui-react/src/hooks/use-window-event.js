a34f6d049d224dd486d6c00cd8afec7f6bc3626e
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-09-21 16:38:26 -0700

    format

--- examples/@headlessui-react/src/hooks/use-window-event.js
+++ examples/@headlessui-react/src/hooks/use-window-event.js
@@ -1,12 +1,12 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react'
 export function useWindowEvent(type, listener, options) {
-    let listenerRef = useRef(listener);
-    listenerRef.current = listener;
-    useEffect(() => {
-        function handler(event) {
-            listenerRef.current.call(window, event);
-        }
-        window.addEventListener(type, handler, options);
-        return () => window.removeEventListener(type, handler, options);
-    }, [type, options]);
+  let listenerRef = useRef(listener)
+  listenerRef.current = listener
+  useEffect(() => {
+    function handler(event) {
+      listenerRef.current.call(window, event)
+    }
+    window.addEventListener(type, handler, options)
+    return () => window.removeEventListener(type, handler, options)
+  }, [type, options])
 }
