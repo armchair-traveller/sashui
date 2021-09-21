89809e8b04864a1e8b8fd6464348606aa8516c74
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-09-21 15:19:05 -0700

    clarify comments

--- examples/@headlessui-react/src/hooks/use-is-initial-render.js
+++ examples/@headlessui-react/src/hooks/use-is-initial-render.js
@@ -1,5 +1,10 @@
 import { onMount } from 'svelte'
 
+/**
+ * * it's likely whatever is calling this can be replaced with afterUpdate, as that only happens after initial render
+ * and onMount only happens for the initial render.
+ * if you ever need reactive onMount or lifecycle funcs, use reactive blocks/declaration ($:)
+ */
 export function useIsInitialRender() {
   let initial = true
 
