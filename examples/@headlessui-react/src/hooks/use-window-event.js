138fdef2edbc9e8180851abf4d361c63916ad00b
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-22 13:56:41 -0700

    use bind instead of call

--- examples/@headlessui-react/src/hooks/use-window-event.js
+++ examples/@headlessui-react/src/hooks/use-window-event.js
@@ -2,9 +2,7 @@ import { onMount } from 'svelte'
 
 export function useWindowEvent(type, listener, options) {
   onMount(() => {
-    function handler(event) {
-      listener.call(window, event)
-    }
+    var handler = listener.bind(window)
     window.addEventListener(type, handler, options)
     return () => window.removeEventListener(type, handler, options)
   })
