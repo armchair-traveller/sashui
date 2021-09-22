b28a8e8161ce7c928c0f6f2134a3eae3b0f56bac
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-22 13:59:12 -0700

    run dispose get only on cleanup

--- examples/@headlessui-react/src/hooks/use-disposables.js
+++ examples/@headlessui-react/src/hooks/use-disposables.js
@@ -5,13 +5,13 @@ import { disposables } from '../utils/disposables'
 
 export function useDisposables() {
   // Using useState instead of useRef so that we can use the initializer function.
-  // ? this was using useState before, should it be changed to a store? Does d need to be reactive?
+  // ? this was using useState before
   // ? my only concern is that it's just functions on RAF so I don't think it should need reactivity...
   /** this is actually something common in this repo, needless use of useState.
    * TODO: Anytime you see use- in the file name, it is likely making wasteful use of useState. Keep this in mind if
    * there's unintended behavior you can check these files later in the backup.
    */
   const d = writable(disposables())
-  onMount(() => get(d).dispose)
+  onMount(() => () => get(d).dispose())
   return d
 }
