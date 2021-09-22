6ce82716e73bfe6422a6454788b80639fc1b60f7
Armchair Traveller <sentient.sober.armchair@gmail.com>
2021-09-21 19:34:25 -0700

    useeffect to onmount

--- examples/@headlessui-react/src/hooks/use-window-event.js
+++ examples/@headlessui-react/src/hooks/use-window-event.js
@@ -1,12 +1,11 @@
-import { useEffect, useRef } from 'react'
+import { onMount } from 'svelte'
+
 export function useWindowEvent(type, listener, options) {
-  let listenerRef = useRef(listener)
-  listenerRef.current = listener
-  useEffect(() => {
+  onMount(() => {
     function handler(event) {
-      listenerRef.current.call(window, event)
+      listener.call(window, event)
     }
     window.addEventListener(type, handler, options)
     return () => window.removeEventListener(type, handler, options)
-  }, [type, options])
+  })
 }
