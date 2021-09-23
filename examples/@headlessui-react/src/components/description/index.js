3a0fc6dd8f2e223056cf39ead8019a538885745c
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-22 18:19:46 -0700

    aggregate and finish imports for description

--- examples/@headlessui-react/src/components/description/index.js
+++ examples/@headlessui-react/src/components/description/index.js
@@ -1,74 +1,31 @@
-import React, {
-  createContext,
-  useCallback,
-  useContext,
-  useMemo,
-  useState,
-} from 'react'
-import { useId } from '../../hooks/use-id'
-import { render } from '../../utils/render'
-import { useIsoMorphicEffect } from '../../hooks/use-iso-morphic-effect'
-let DescriptionContext = createContext(null)
-function useDescriptionContext() {
-  let context = useContext(DescriptionContext)
-  if (context === null) {
-    let err = new Error(
-      'You used a <Description /> component, but it is not inside a relevant parent.'
-    )
-    if (Error.captureStackTrace)
-      Error.captureStackTrace(err, useDescriptionContext)
-    throw err
-  }
-  return context
-}
 export function useDescriptions() {
-  let [descriptionIds, setDescriptionIds] = useState([])
+  // * because svelte context doesn't specifically wrap, it just uses a key, we don't have a wrapper component
+  // * just use this in whatever component you want as provider
+  // * from my perspective, each only has one provider, and won't really conflict with each other, so it
+  // * should be fine to use Svelte's context
+  let sDescriptionIds = writable([])
   return [
-    // The actual id's as string or undefined
-    descriptionIds.length > 0 ? descriptionIds.join(' ') : undefined,
+    // Store: The actual id's as string or undefined.
+    derived(sDescriptionIds, (descriptionIds) =>
+      descriptionIds.length > 0 ? descriptionIds.join(' ') : undefined
+    ),
     // The provider component
-    useMemo(() => {
-      return function DescriptionProvider(props) {
-        let register = useCallback((value) => {
-          setDescriptionIds((existing) => [...existing, value])
-          return () =>
-            setDescriptionIds((existing) => {
-              let clone = existing.slice()
-              let idx = clone.indexOf(value)
-              if (idx !== -1) clone.splice(idx, 1)
-              return clone
-            })
-        }, [])
-        let contextBag = useMemo(
-          () => ({
-            register,
-            slot: props.slot,
-            name: props.name,
-            props: props.props,
-          }),
-          [register, props.slot, props.name, props.props]
-        )
-        return React.createElement(
-          DescriptionContext.Provider,
-          { value: contextBag },
-          props.children
-        )
+    function setDescriptionContext() {
+      let register = (value) => {
+        sDescriptionIds.update((existing) => [...existing, value])
+        return () =>
+          sDescriptionIds.update((existing) => {
+            let clone = existing.slice()
+            let idx = clone.indexOf(value)
+            if (idx !== -1) clone.splice(idx, 1)
+            return clone
+          })
       }
-    }, [setDescriptionIds]),
+      // * this provider sets the context value to the register func
+      setContext('description', register)
+    },
   ]
 }
-// ---
-let DEFAULT_DESCRIPTION_TAG = 'p'
-export function Description(props) {
-  let context = useDescriptionContext()
-  let id = `headlessui-description-${useId()}`
-  useIsoMorphicEffect(() => context.register(id), [id, context.register])
-  let passThroughProps = props
-  let propsWeControl = { ...context.props, id }
-  return render({
-    props: { ...passThroughProps, ...propsWeControl },
-    slot: context.slot || {},
-    defaultTag: DEFAULT_DESCRIPTION_TAG,
-    name: context.name || 'Description',
-  })
-}
+
+import _Description from './Description.svelte'
+export const Description = _Description
