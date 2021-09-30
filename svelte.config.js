771297796a44257c5c3ecb79ef79c9db7db1f47e
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-30 04:57:37 -0700

    add tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
+import preprocess from 'svelte-preprocess'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 
-export default config;
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}
+
+export default config
