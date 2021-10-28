6a868c92ac4dc89a701ecf59a2e02cc16d0c95be
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-10-28 02:31:53 -0700

    add some context to why frontmost was added and why it won't be used (for now)

--- src/lib/stores/frontmost.js
+++ src/lib/stores/frontmost.js
@@ -1,3 +1,5 @@
+// ? This was created to deal with nested modals, however a nested modal is a UX antipattern.
+// ? Current implementation of modals will not include nesting support (though I'm sure it'd still be possible for a consumer to do it, albeit with some difficulty)
 import { derived, writable } from 'svelte/store'
 
 const openCount = writable(0)
