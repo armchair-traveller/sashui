030c50e9356ca328f1dabcf3fc72891737001409
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-10-28 01:19:00 -0700

    refactor frontmost to subscription only

--- src/lib/stores/frontmost.js
+++ src/lib/stores/frontmost.js
@@ -3,14 +3,17 @@ import { derived, writable } from 'svelte/store'
 const openCount = writable(0)
 /** This derived store keeps track of currently active Sash component as a boolean. By using provided API, Sash
  * components can determine whether they're the active (most recent) component or not, which is important for nesting.
- * e.g. deciding whether certain events should be triggered if the specific component is not active.
+ * e.g. guarding certain events if the current component is not in the forefront.
  * On subscribe, adds to count, thus should only be subscribed once per sash component. On unsubscribe, decrements count.
  */
-export function frontmost() {
-  var curN
-  openCount.update((count) => (curN = ++count))
-  return {
-    subscribe: (cb) =>
-      derived(openCount, (count) => count == curN).subscribe(cb, () => openCount.update((count) => --count)),
-  }
+export const frontmost = {
+  subscribe(cb) {
+    var curN
+    openCount.update((count) => (curN = ++count))
+    const unsub = derived(openCount, (count) => count == curN).subscribe(cb)
+    return function unsubscriber() {
+      unsub()
+      openCount.update((count) => (curN = --count))
+    }
+  },
 }
