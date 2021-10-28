a561805909cebca9f03ced9f894da6754b5b106a
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-10-28 07:29:27 -0700

    inert nestedMenu test

---
+++ src/routes/nestedMenu.svelte
@@ -0,0 +1,23 @@
+<script>
+import { useMenu } from '$lib'
+const Menu = useMenu()
+const NestedMenu = useMenu()
+</script>
+
+<button use:Menu.button> Open Menu </button>
+
+{#if $Menu}
+  <menu use:Menu>
+    <Menu.Item nested>
+      <button use:NestedMenu.button>Open Nested Menu</button>
+    </Menu.Item>
+  </menu>
+{/if}
+
+{#if $NestedMenu}
+  <menu use:NestedMenu>
+    <NestedMenu.Item>
+      <button>Nested Menu Item</button>
+    </NestedMenu.Item>
+  </menu>
+{/if}
