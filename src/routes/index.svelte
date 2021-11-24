b59a5c7bbaf5fd51384873a217c661b659153089
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-11-24 11:19:47 -0800

    add examples in routes

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to sashui</h1>
+
+<h2>See demos in action below (and browse their route code to see the API or check out the docs)</h2>
+
+<ul>
+  <a href="/menu" sveltekit:prefetch>Menu</a>
+  <a href="/dialog" sveltekit:prefetch>Dialog</a>
+</ul>
