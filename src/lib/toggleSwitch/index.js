0886b990e9110cc77da7d2b0f7029d9bc2e98b45
armchair-traveller <sentient.sober.armchair@gmail.com>
2022-04-25 09:04:25 -0700

    remove declaration types, add action jsdoc types for toggle. In TS

--- src/lib/toggleSwitch/index.js
+++ src/lib/toggleSwitch/index.js
@@ -1,37 +1,6 @@
-import { addEvts } from './utils/action'
+import { addEvts } from '../utils/action'
 
-/**
-  Markup:
-  button
-  label (optional)
-  group (optional, use description which isn't actually present in the markup, just for context state)
-  
-  Toggle (button)
-  props + events:
-  id, // `headlessui-switch${$useId}`
-  role: 'switch',
-  tabIndex: 0,
-  'aria-checked': checked, // boolean, exposed prop
-  onClick: handleClick,
-  (event) => {
-    event.preventDefault()
-    toggle()
-    let toggle = () => onChange(!checked)
-  }
-  onKeyUp: handleKeyUp,
-  (event) => {
-    if (event.key !== Keys.Tab) event.preventDefault()
-    if (event.key === Keys.Space) toggle()
-  }
-  onKeyPress: handleKeyPress,
-  // This is needed so that we can "cancel" the click event when we use the `Enter` key on a button.
-  (event) => event.preventDefault()
-  
-  unnecessary props
-  type: useResolveButtonType(props, internalSwitchRef), // irrelevant
-  'aria-labelledby': groupContext?.labelledby,
-  'aria-describedby': groupContext?.describedby,
-*/
+/** @typedef {import('svelte/action').Action<HTMLButtonElement | HTMLInputElement | HTMLElement, boolean>} Toggle */
 
 /** Switch action
  *
@@ -49,7 +18,7 @@ import { addEvts } from './utils/action'
  * `role="button" tabindex="0"`. In this scenario, `<label>`'s click event will also not work according to normal usage,
  * so if click behavior is desired just manually add a click event to set the press state.
  */
-export const Switch = createSwitchToggle(false)
+export const toggle = createToggleSwitch()
 /** Toggle action
  *
  * Pass in current pressed state.
@@ -66,21 +35,22 @@ export const Switch = createSwitchToggle(false)
  * `role="button" tabindex="0"`. In this scenario, `<label>`'s click event will also not work according to normal usage,
  * so if click behavior is desired just manually add a click event to set the press state.
  */
-export const toggle = createSwitchToggle()
+export const Switch = createToggleSwitch(false)
 
-function createSwitchToggle(isSwitch = true) {
-  const aria = `aria-${isSwitch ? 'checked' : 'pressed'}`
+function createToggleSwitch(isToggle = true) {
+  const aria = `aria-${isToggle ? 'pressed' : 'checked'}`
 
+  /** @type {Toggle} */
   return function Switch(el, checked = false) {
-    isSwitch && el.setAttribute('role', 'switch')
+    !isToggle && el.setAttribute('role', 'switch')
     update(checked)
 
-    // ? Could easily paas in the event that caused the change to detail, but I don't see the point of doing so. Needs use case.
+    // ? Could easily pass in the event that caused the change to detail, but I don't see the point of doing so. Needs use case.
     function toggle() {
       el.dispatchEvent(new CustomEvent('change', { detail: !checked }))
     }
-    function update(curChecked = checked) {
-      el.setAttribute(aria, !!(checked = curChecked))
+    function update(_checked = checked) {
+      el.setAttribute(aria, !!(checked = _checked))
     }
     return {
       update,
